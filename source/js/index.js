40e151f028cd6a9e13255532d4b1aa243fbcc41d
boseny <yxy43445@gmail.com>
2018-01-24 18:22:42 +0800

    add run top btn

--- source/js/index.js
+++ source/js/index.js
@@ -1,30 +1,41 @@
+var $body = document.body;
+var $toggle = document.querySelector(".navbar-toggle");
+var $navbar = document.querySelector("#bosenyblog-navbar");
+var $collapse = document.querySelector(".navbar-collapse");
+var topBtn = document.querySelector(".gotop-btn");
 
-// let $toggle = document.querySelector('.navbar-toggle')
-
-// $toggle.addEventListener("click", (e) => {
-//     e.preventDefault()
-//     console.log(12121)
-// })
-var $body   = document.body;
-    var $toggle = document.querySelector('.navbar-toggle');
-    var $navbar = document.querySelector('#bosenyblog-navbar');
-    var $collapse = document.querySelector('.navbar-collapse');
-
-    $toggle.addEventListener('click', handleMagic)
-    function handleMagic(e){
-        if ($navbar.className.indexOf('in') > 0) {
-        // CLOSE
-            $navbar.className = "";
-            // wait until animation end.
-            setTimeout(function(){
-                // prevent frequently toggle
-                if($navbar.className.indexOf('in') < 0) {
-                    $collapse.style.height = "0px"
-                }
-            },400)
-        }else{
-        // OPEN
-            $collapse.style.height = "auto"
-            $navbar.className += " in";
-        }
-    }
+$toggle.addEventListener("click", handleMagic);
+function handleMagic(e) {
+  if ($navbar.className.indexOf("in") > 0) {
+    // CLOSE
+    $navbar.className = "";
+    // wait until animation end.
+    setTimeout(function() {
+      // prevent frequently toggle
+      if ($navbar.className.indexOf("in") < 0) {
+        $collapse.style.height = "0px";
+      }
+    }, 400);
+  } else {
+    // OPEN
+    $collapse.style.height = "auto";
+    $navbar.className += " in";
+  }
+}
+$(window).scroll(function() {
+  $(this).scrollTop() > 400
+    ? $(".gotop-btn").css("display", "block")
+    : $(".gotop-btn").hide();
+});
+$(".gotop-btn").hover(
+  function() {
+    $(this).addClass("top-active");
+  },
+  function() {
+    $(this).removeClass("top-active");
+  }
+);
+topBtn.addEventListener("click", () => {
+  $("html,body").animate({ scrollTop: "0px" }, 500);
+  $(".gotop-btn").removeClass("top-active");
+});
