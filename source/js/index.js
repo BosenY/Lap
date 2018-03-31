7332ef06364e5acc7532e027f30643ec2b1fd721
boseny <a707328818@qq.com>
2018-04-01 02:19:39 +0800

    add catalog module

--- source/js/index.js
+++ source/js/index.js
@@ -3,6 +3,11 @@ var $toggle = document.querySelector(".navbar-toggle");
 var $navbar = document.querySelector("#bosenyblog-navbar");
 var $collapse = document.querySelector(".navbar-collapse");
 var topBtn = document.querySelector(".gotop-btn");
+var $toc = document.querySelector("#toc")
+
+var win=$(window); //得到窗口对象
+var sc=$(document);//得到document文档对象。
+var headerHeight = $('.header-container').height()
 
 $toggle.addEventListener("click", handleMagic);
 function handleMagic(e) {
@@ -26,6 +31,16 @@ $(window).scroll(function() {
   $(this).scrollTop() > 400
     ? $(".gotop-btn").css("display", "block")
     : $(".gotop-btn").hide();
+    var $catalog = $('#toc');
+    // $catalog.show()
+    if ($(this).scrollTop() > (headerHeight + 95)) {
+      console.log($(this).scrollTop())
+        $catalog.addClass('fixed')
+        // $catalog.removeAttr("left")
+    } else {
+        $catalog.removeClass('fixed')
+        // $catalog.css("left", "15%")
+    }
 });
 $(".gotop-btn").hover(
   function() {
@@ -39,35 +54,7 @@ topBtn.addEventListener("click", () => {
   $("html,body").animate({ scrollTop: "0px" }, 500);
   $(".gotop-btn").removeClass("top-active");
 });
-// var vm = new Vue({
-//   el: "#vue",
-//   data: {
-//     text: ""
-//   },
-//   computed: {},
-//   methods: {},
-//   mounted() {
-//     $.ajax({
-//       url: "https://boseny-blog.disqus.com/embed.js", //请求的URL
-//       timeout: 3000, //超时时间设置，单位毫秒
-//       type: "get", //请求方式，get或post
-//       data: {}, //请求所传参数，json格式
-//       dataType: "script", //返回的数据格式
-//       success: function(data) {
-//         //请求成功的回调函数
-//         return;
-//       },
-//       error: function(err) {
-//         vm.text = "温馨提示，评论需要全局代理才可使用";
-//         return;
-//       },
-//       complete: function(XMLHttpRequest, status) {
-//         //请求完成后最终执行参数
-//         if (status == "timeout") {
-//           //超时,status还有success,error等值的情况
-//           return;
-//         }
-//       }
-//     });
-//   }
-// });
+if($toc.childElementCount <= 1) {
+  $toc.style.display = "none"
+}
+
